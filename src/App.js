8da96c9b960ed063b2dd3785f982389057183906
Billie Rose <billie.rose@zywave.com>
2020-02-22 16:28:20 -0600

    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './assets/styles/index.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./assets/styles/index.css";
+import Carousel from "react-bootstrap/Carousel";
 
 function App() {
-
   return (
-
     <div>
-      <p className="questions">Pick your favorite color?</p>
-      <p className="questions">how are you feeling today?</p>
-      <p className="questions">Do you feel safe today?</p>
-      <p className="questions">Do you feel safe to talk here?</p>
-    
+      <Carousel interval={null}>
+        <Carousel.Item>
+          <div className="questions">Pick your favorite color?</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="questions">how are you feeling today?</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="questions">Do you feel safe today?</div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="questions">Do you feel safe to talk here?</div>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
